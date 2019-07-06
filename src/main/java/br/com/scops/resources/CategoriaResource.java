0964eaae5a5def2efb7e1f57ea18ed955e6560fe
scarpah600 <gustavo.scarpin.2020@gmail.com>
2019-07-06 20:02:12 -0300

    teste

--- src/main/java/br/com/scops/resources/CategoriaResource.java
+++ src/main/java/br/com/scops/resources/CategoriaResource.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
-
+//gustavo
 	@RequestMapping(method = RequestMethod.GET)
 	public String listar() {
 		return "Desenvolvimento pela empresa Scops";
