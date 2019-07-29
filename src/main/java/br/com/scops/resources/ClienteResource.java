178c85d46c1edb1948a227638c7e57b8617af789
scarpah600 <gustavo.scarpin.2020@gmail.com>
2019-07-29 18:16:30 -0300

    PUT,DELETE E GET PARA CLIENTE

--- src/main/java/br/com/scops/resources/ClienteResource.java
+++ src/main/java/br/com/scops/resources/ClienteResource.java
@@ -1,13 +1,22 @@
 package br.com.scops.resources;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.scops.domain.Cliente;
+import br.com.scops.dto.ClienteDTO;
 import br.com.scops.servicies.ClienteService;
 
 @RestController
@@ -24,5 +33,38 @@ public class ClienteResource {
 		return ResponseEntity.ok().body(obj);
 		
 	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Void> alterar(@Valid @RequestBody ClienteDTO objDto, @PathVariable Integer id) {
+		Cliente obj = service.fromDTO(objDto);
+		obj.setId(id);
+		obj = service.alterando(obj);
+		return ResponseEntity.noContent().build();
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Void> deletar(@PathVariable Integer id) {
+		service.deletar(id);
+		return ResponseEntity.noContent().build();
+	}
+
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<ClienteDTO>> buscaTodos() {
+		List<Cliente> lista = service.buscarTodos();
+		List<ClienteDTO> listaDto = lista.stream().map(obj -> new ClienteDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listaDto);
+
+	}
 	
+	@RequestMapping(value="/page",method = RequestMethod.GET)
+	public ResponseEntity<Page<ClienteDTO>> buscaPagina(
+			@RequestParam(value="page",defaultValue = "0")Integer page, 
+			@RequestParam(value="linesPerpage",defaultValue = "24")Integer linesPerpage,
+			@RequestParam(value="orderBy",defaultValue = "0")String orderBy, 
+			@RequestParam(value="direction",defaultValue = "0")String direction) {
+		Page<Cliente> lista = service.buscarPagina(page, linesPerpage, orderBy, direction);
+		Page<ClienteDTO> listaDto = lista.map(obj -> new ClienteDTO(obj));
+		return ResponseEntity.ok().body(listaDto);
+
+	}
 }
--- src/main/java/br/com/scops/servicies/ClienteService.java
+++ src/main/java/br/com/scops/servicies/ClienteService.java
@@ -1,12 +1,18 @@
 package br.com.scops.servicies;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import br.com.scops.dao.ClienteDAO;
 import br.com.scops.domain.Cliente;
+import br.com.scops.dto.ClienteDTO;
 
 @Service
 public class ClienteService {
@@ -20,5 +26,39 @@ public class ClienteService {
     	return obj.orElseThrow(() -> new br.com.scops.servicies.ObjectNotFoundException(
     			"Objeto não Encontrado ! id" + id + ",Tipo: " + Cliente.class.getName()));
     }
-		
+    
+	public Cliente alterando(Cliente obj) {
+		Cliente newObj = buscar(obj.getId());
+		updateDate(newObj, obj);
+		return dao.save(newObj);
+	}
+
+	public void deletar(Integer id) {
+		buscar(id);
+		try {
+			dao.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possivel excluir um cliente que possui produtos");
+		}
+	}
+
+	public List<Cliente> buscarTodos() {
+		return dao.findAll();
+	}
+
+	public Page<Cliente> buscarPagina(Integer page, Integer linesPerpage, String orderBy, String direction) {
+
+		PageRequest pageRequest = PageRequest.of(page, linesPerpage, Direction.valueOf(direction), 
+				orderBy);
+		return dao.findAll(pageRequest);
+	}
+	public Cliente fromDTO(ClienteDTO objtDtO) {
+          return new Cliente(objtDtO.getId(), objtDtO.getNome(),
+        		  objtDtO.getEmail(), null, null);
+	}
+	
+	private void updateDate(Cliente newobj,Cliente obj) {
+		newobj.setNome(obj.getNome());
+		newobj.setEmail(obj.getEmail());
+	}
 }
