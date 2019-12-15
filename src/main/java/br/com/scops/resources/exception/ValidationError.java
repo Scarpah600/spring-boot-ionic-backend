6c1218ea3c321709cabf7ac5c15a3e93c8b72fdd
Scarpah600 <gustavo.scarpin.2020@gmail.com>
2019-12-15 10:59:56 -0300

    padrao de exeçõeserros

--- src/main/java/br/com/scops/resources/exception/ValidationError.java
+++ src/main/java/br/com/scops/resources/exception/ValidationError.java
@@ -5,12 +5,11 @@ import java.util.List;
 
 public class ValidationError extends StandardError {
 	private static final long serialVersionUID = 1L;
-	
+
 	private List<FieldMessage> errors = new ArrayList<>();
-	
-	public ValidationError(Integer status, String msg, Long timeStamp) {
-		super(status, msg, timeStamp);
-		
+
+	public ValidationError(Long timeStamp, Integer status, String error, String message, String path) {
+		super(timeStamp, status, error, message, path);
 	}
 
 	public List<FieldMessage> getErros() {
@@ -18,10 +17,7 @@ public class ValidationError extends StandardError {
 	}
 
 	public void addError(String fieldName, String messagem) {
-		errors.add(new FieldMessage(fieldName,messagem));
+		errors.add(new FieldMessage(fieldName, messagem));
 	}
-	
-
-	
 
 }
